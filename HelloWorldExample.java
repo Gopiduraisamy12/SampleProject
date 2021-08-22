7d5984ebeee8481a5d80ede3fa7a642bfbcbf5e8
Gopiduraisamy12 <89338715+Gopiduraisamy12@users.noreply.github.com>
2021-08-22 17:30:54 +0530

    Update HelloWorldExample.java

--- HelloWorldExample.java
+++ HelloWorldExample.java
@@ -1,7 +1,7 @@
  public class HelloWorldExample {
     public static void main(String[] args) {
         System.out.println("Hello world");
-     System.out.println("Hello world !!");
-     System.out.println("Hello world !!");
+        System.out.println("Hello world !!");
+        System.out.println("Hello world !!");
     }
 }
