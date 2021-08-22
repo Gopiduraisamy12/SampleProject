227d35dbf3abf55cd2f21fe141397880ed66e248
Gopiduraisamy12 <89338715+Gopiduraisamy12@users.noreply.github.com>
2021-08-22 17:32:32 +0530

    Update HelloWorldExample.java

--- HelloWorldExample.java
+++ HelloWorldExample.java
@@ -3,6 +3,6 @@
         System.out.println("Hello world");
         System.out.println("Hello world !!");
         System.out.println("Hello world !!");
-        System.out.println("Hello world !!");
+        System.out.println("Hello world");
     }
 }
